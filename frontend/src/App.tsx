d642d32d0540ac1498190ce5707cda0713786bac
VitorRayan <vitor_rayan@hotmail.com>
2026-05-11 01:00:52 -0300

    chore: restructure project into backend/frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>CRM Project</h1>
+
+      <h2>Clientes</h2>
+
+      <button>Cadastrar Cliente</button>
+    </div>
+  )
+}
+
+export default App
--- src/clients/dto/create-client.dto.ts
+++
@@ -1,4 +0,0 @@
-export class CreateClientDto {
-  nome: string;
-  email: string;
-}
